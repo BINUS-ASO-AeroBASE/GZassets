2b6fe843f007e6f0d6acf30912f27228da55721f
Gareth <theonegareth@gmail.com>
2025-04-24 17:42:40 +0700

    clearing files

--- install/build/drone_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aerobase/drone_model/src/drone_description
-# Build directory: /home/aerobase/drone_model/build/drone_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
